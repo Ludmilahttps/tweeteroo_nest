814d6d9eef82cef6a416076f0f0574c4b2a264c6
LudmilaS <ludmila.https@gmail.com>
2023-08-21 11:56:04 -0300

    feat:update

--- src/app.service.ts
+++ src/app.service.ts
@@ -6,77 +6,79 @@ import { CreateTweetDto } from './dtos/tweet-dto';
 
 @Injectable()
 export class AppService {
+
   getHello(): string {
     return "I'm okay!";
   }
-
+  
   private users: User[]
   private tweets: Tweet[]
 
-  constructor() {
-    this.users = []
-    this.tweets = []
+  constructor(){
+    this.users=[]
+    this.tweets=[]
   }
 
-  getTweetByName(username: string) {
-    let arr = []
-    for (let i = 0; i < this.tweets.length; i++) {
-      if (this.tweets[i].getUsername() === username) {
+  getTweetByName(username:string) {
+    let arr=[]
+    for(let i=0;i<this.tweets.length;i++){
+      if(this.tweets[i].getUsername()===username){
         arr.push({
-          username: this.tweets[i].getUsername(),
-          avatar: this.tweets[i].getAvatar(),
-          tweet: this.tweets[i].getTweet()
-        })
+          username:this.tweets[i].getUsername(),
+          avatar:this.tweets[i].getAvatar(),
+          tweet:this.tweets[i].getTweet()})
       }
     }
     return arr
   }
-
-  postSign(body: CreateSignUpDto) {
-    const { username, avatar } = body
-    return this.users.push(new User(username, avatar));
+  
+  postSign(body:CreateSignUpDto){
+    const {username,avatar}=body
+    return this.users.push(new User(username,avatar));
   }
 
-  postTweets(body: CreateTweetDto) {
-    const { username, tweet } = body
-    let avatar = "false"
-    for (let i = 0; i < this.users.length; i++) {
-      if (this.users[i].getUsername() === username) {
-        avatar = this.users[i].getAvatar()
+  postTweets(body:CreateTweetDto){
+    const {username,tweet}=body
+    let avatar="false"
+    for(let i=0;i<this.users.length;i++){
+      if(this.users[i].getUsername()===username){
+        avatar=this.users[i].getAvatar()
       }
     }
 
-    if (avatar === "false")
-      throw new Error("unauthorized")
+    if(avatar==="false")
+    throw new Error("unauthorized")
 
-    return this.tweets.push(new Tweet(new User(username, avatar), tweet));
+    return this.tweets.push(new Tweet(new User(username,avatar),tweet)); 
   }
+
   getTweets(page: number) {
     if (page <= 0) {
-      throw new Error("O número da página deve ser maior que zero");
+        throw new Error("O número da página deve ser maior que zero");
     }
-    let arr = []
-    if (page) {
-      const tweetsPerPage = 15;
-      const startIndex = (page - 1) * tweetsPerPage;
-      const endIndex = startIndex + tweetsPerPage;
+    let arr=[]
+    if(page){
+    const tweetsPerPage = 15;
+    const startIndex = (page - 1) * tweetsPerPage;
+    const endIndex = startIndex + tweetsPerPage;
 
       for (let i = startIndex; i < endIndex && i < this.tweets.length; i++) {
-        arr.push({
-          username: this.tweets[i].getUsername(),
-          avatar: this.tweets[i].getAvatar(),
-          tweet: this.tweets[i].getTweet()
-        });
+          arr.push({
+              username: this.tweets[i].getUsername(),
+              avatar: this.tweets[i].getAvatar(),
+              tweet: this.tweets[i].getTweet()
+          });
       }
-    } else {
-      for (let i = 0; i < 15; i++) {
+    }else{
+      for (let i = 0; i < 15;i++) {
         arr.push({
-          username: this.tweets[i].getUsername(),
-          avatar: this.tweets[i].getAvatar(),
-          tweet: this.tweets[i].getTweet()
+            username: this.tweets[i].getUsername(),
+            avatar: this.tweets[i].getAvatar(),
+            tweet: this.tweets[i].getTweet()
         });
       }
     }
     return arr;
-  }
+}
+
 }
